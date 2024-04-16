313bf03b84df0b71677246b8550db6ff8ad4ffab
Marcelo Cristiano <marcelo-nc1@hotmail.com>
2024-04-16 16:03:31 -0300

    Correção de erros

--- scraping.py
+++ scraping.py
@@ -4,38 +4,12 @@ from selenium.webdriver.support.ui import Select
 import time
 import os
 import requests
-<<<<<<< HEAD
-<<<<<<< HEAD
 import pandas as pd
-=======
-=======
->>>>>>> 710c25ab4348aaf9ce71cecd8b671f930168c020
+
 
 def download_arquivo(ano_ini, ano_final, tipo_pessoa):
-    # Configurações para o download do arquivo
-    prefs = {
-        "download.name": "teste",
-        "download.default_directory": r"C:\Projetos-Python\ministerio_da_cultura\repository\bronze\ministerio da cultura\{}".format(tipo_pessoa)
-    }
     options = webdriver.ChromeOptions()
-    options.add_experimental_option("prefs", prefs)
     options.add_argument('--headless')
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
-
-    # Inicializando o driver do Chrome
-    driver = webdriver.Chrome(options=options)
-
-<<<<<<< HEAD
-def download_arquivo(ano_ini, ano_final, tipo_pessoa):
-    # Configurações para o download do arquivo
-    prefs = {
-        "download.name": "teste",
-        "download.default_directory": r"C:\Projetos-Python\ministerio_da_cultura\repository\bronze\ministerio da cultura\{}".format(tipo_pessoa)
-    }
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("prefs", prefs)
-    #options.add_argument('--headless')
 
     # Inicializando o driver do Chrome
     driver = webdriver.Chrome(options=options)
@@ -43,11 +17,6 @@ def download_arquivo(ano_ini, ano_final, tipo_pessoa):
     # URL do site
     url = "http://sistemas.cultura.gov.br/salicnet/ctrIncentivadorMecenatoPorRegiaoUF/ctrIncentivadorMecenatoPorRegiaoUF.php"
 
-=======
-    # URL do site
-    url = "http://sistemas.cultura.gov.br/salicnet/ctrIncentivadorMecenatoPorRegiaoUF/ctrIncentivadorMecenatoPorRegiaoUF.php"
-
->>>>>>> refs/remotes/origin/main
     # Iterando sobre os anos especificados
     for ano in range(ano_ini, ano_final + 1):
         msg = "Download ano " + str(ano)
@@ -77,11 +46,7 @@ def download_arquivo(ano_ini, ano_final, tipo_pessoa):
         url_dowload = "http://sistemas.cultura.gov.br{}".format(link_download)
 
         # Diretório base para salvar o arquivo
-<<<<<<< HEAD
         diretorio_base = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\bronze\ministerio da cultura"
-=======
-        diretorio_base = r"C:\Projetos-Python\ministerio_da_cultura\repository\bronze\ministerio da cultura"
->>>>>>> refs/remotes/origin/main
         nome_pasta = tipo_pessoa
 
         # Caminho da pasta para o tipo de pessoa
@@ -105,14 +70,8 @@ def download_arquivo(ano_ini, ano_final, tipo_pessoa):
 
     # Fechando o driver do Chrome
     driver.quit()
-<<<<<<< HEAD
-=======
->>>>>>> 710c25ab4348aaf9ce71cecd8b671f930168c020
 
-    # Inicializando o driver do Chrome
-    driver = webdriver.Chrome(options=options)
 
-<<<<<<< HEAD
 def transformar_dado_silver(tipo_pessoa):
     diretorio_bronze = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\bronze\ministerio da cultura\{}".format(tipo_pessoa)
     diretorio_silver = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\silver\ministerio da cultura\{}".format(tipo_pessoa)
@@ -160,67 +119,7 @@ def transformar_dado_gold(tipo_pessoa):
         # Adicionar o DataFrame à lista
         dfs.append(df)
 
-        # Concatenar os DataFrames em um único DataFrame
-        df = pd.concat(dfs, ignore_index=True)
-        # Salva o DataFrame como arquivo Parquet
-        df.to_parquet(os.path.join(diretorio_gold, arquivo_parquet))
-=======
->>>>>>> refs/remotes/origin/main
-=======
-    # URL do site
-    url = "http://sistemas.cultura.gov.br/salicnet/ctrIncentivadorMecenatoPorRegiaoUF/ctrIncentivadorMecenatoPorRegiaoUF.php"
-
-    # Iterando sobre os anos especificados
-    for ano in range(ano_ini, ano_final + 1):
-        msg = "Download ano " + str(ano)
-
-        # Abrindo a página no navegador
-        driver.get(url=url)
-        time.sleep(2)
-
-        # Selecionando o ano e o tipo de pessoa nos dropdowns
-        ano_element = driver.find_element(By.NAME, "ano")
-        select_ano = Select(ano_element)
-        select_ano.select_by_visible_text(str(ano))
-
-        tipo_pessoa_element = driver.find_element(By.NAME, "tipopessoa")
-        select_tipo_pessoa = Select(tipo_pessoa_element)
-        select_tipo_pessoa.select_by_visible_text(tipo_pessoa)
-
-        # Clicando no botão "OK"
-        btn_ok = driver.find_element(By.NAME, "sub_form").click()
-        time.sleep(1)
-
-        # Clicando no botão para baixar o arquivo XLS
-        btn_xls = driver.find_element(By.NAME, "sc_b_xls").click()
-
-        # Obtendo o link de download do arquivo
-        link_download = driver.find_element(By.XPATH, '/html/body/font/a/font/b').text
-        url_dowload = "http://sistemas.cultura.gov.br{}".format(link_download)
-
-        # Diretório base para salvar o arquivo
-        diretorio_base = r"C:\Projetos-Python\ministerio_da_cultura\repository\bronze\ministerio da cultura"
-        nome_pasta = tipo_pessoa
-
-        # Caminho da pasta para o tipo de pessoa
-        path_pessoa_juridica = os.path.join(diretorio_base, nome_pasta)
-
-        # Verificando e criando a pasta se ela não existir
-        if not os.path.exists(path_pessoa_juridica):
-            os.makedirs(path_pessoa_juridica)
-
-        # Caminho completo do arquivo
-        caminho_arquivo = os.path.join(path_pessoa_juridica, str(ano) + '.xls')
-
-        # Baixando e salvando o arquivo
-        resp = requests.get(url_dowload)
-        with open(caminho_arquivo, 'wb') as arq:
-            arq.write(resp.content)
-        time.sleep(3)
-
-        # Retornando mensagem
-        yield msg
-
-    # Fechando o driver do Chrome
-    driver.quit()
->>>>>>> 710c25ab4348aaf9ce71cecd8b671f930168c020
+    # Concatenar os DataFrames em um único DataFrame
+    df = pd.concat(dfs, ignore_index=True)
+    # Salva o DataFrame como arquivo Parquet
+    df.to_parquet(os.path.join(diretorio_gold, arquivo_parquet))
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Projetos-Python\ministerio_da_cultura\venv
+set VIRTUAL_ENV=C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
