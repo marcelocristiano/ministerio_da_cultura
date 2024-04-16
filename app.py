629befd6c549afcd61c6051b5d1ac823f6188669
Marcelo Cristiano <marcelo-nc1@hotmail.com>
2024-04-16 15:18:53 -0300

    .

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import PhotoImage
 import datetime
+import scraping
+import time
 
 app = ttk.Window(themename='flatly')
 app.resizable(width=False, height=False)
@@ -9,12 +11,25 @@ app.iconbitmap('img/brasil.ico')
 app.title('Ministerio da Cultura')
 app.geometry('800x500')
 
-def busca_dado():
-    ano_ini = c_ano_ini.get()
-    ano_fim = c_ano_fim.get()
-    tipo_pessoa = (0 if c_tipo_pessoa.get() == 'Jurídica' else 1)
 
-    print(ano_ini, ano_fim, tipo_pessoa)
+def busca_dado():
+    ano_ini = int(c_ano_ini.get())
+    ano_fim = int(c_ano_fim.get())
+    tipo_pessoa = c_tipo_pessoa.get()
+    l_msg.config(text=None)
+    l_msg.update_idletasks()
+    l_msg.config(text='Iniciando...')
+    l_msg.update_idletasks()
+    for i in scraping.download_arquivo(ano_ini, ano_fim, tipo_pessoa):
+        l_msg.config(text= i)
+        app.update_idletasks()
+    l_msg.config(text='Dowloads Concluídos')
+    for j in scraping.transformar_dado_silver(tipo_pessoa):
+        l_msg.config(text= i)
+        app.update_idletasks()
+        l_msg.config(text='Processando Dados...')
+    scraping.transformar_dado_gold(tipo_pessoa)
+    l_msg.config(text='Processo Concluídos')
 
 # Carrega a imagem
 image = PhotoImage(file='img/logo-govbr.png')  # Substitua com o caminho correto da sua imagem
@@ -52,14 +67,17 @@ c_ano_fim.grid(column=2, row=1, padx=5, pady=5, sticky='w')
 l_tipo_pessoa = ttk.Label(f_ano, text='Tipo de Pessoa:', font=("Arial", 10))
 l_tipo_pessoa.grid(column=1, row=2, sticky='w')
 
-l_tipo_pessoa = ['Física', 'Jurídica']
+l_tipo_pessoa = ['Pessoa Jurídica']
 # Criando uma caixa de combinação para selecionar o tipo de pessoa, alinhada à esquerda
-c_tipo_pessoa = ttk.Combobox(f_ano, width=7, values=l_tipo_pessoa)
+c_tipo_pessoa = ttk.Combobox(f_ano, width=13, values=l_tipo_pessoa)
 c_tipo_pessoa.grid(column=2, row=2, padx=5, pady=5, sticky='w')
 
 # Criando um botão "Buscar" com estilo 'info' e largura 20 na aplicação principal
 b1 = ttk.Button(app, text="Buscar", style='info', width=20, command=busca_dado)
-b1.pack(padx=10, pady=20)
+b1.pack(padx=10, pady=10)
+
+l_msg = ttk.Label(app, font=("Arial", 10,), foreground="red")
+l_msg.pack()
 
 # Executando o loop principal da aplicação
 if __name__ == '__main__':
--- scraping.py
+++ scraping.py
@@ -1,15 +1,131 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import os
 import requests
-from bs4 import BeautifulSoup
-
-payload = {
-'nmgp_parms': 'ano?#?1994?@?tipopessoa?#?2?@?NM_btn_insert?#?S?@?NM_btn_update?#?S?@?NM_btn_delete?#?S?@?NM_btn_navega?#?S?@?',
-'nmgp_url_saida': '/salicnet/ctrIncentivadorMecenatoPorRegiaoUF/ctrIncentivadorMecenatoPorRegiaoUF.php',
-'script_case_init': 1
-}
+import pandas as pd
 
 
-url= 'http://sistemas.cultura.gov.br/salicnet/conIncentivadorMecenatoPorRegiaoUF/conIncentivadorMecenatoPorRegiaoUF.php'
+def download_arquivo(ano_ini, ano_final, tipo_pessoa):
+    # Configurações para o download do arquivo
+    prefs = {
+        "download.name": "teste",
+        "download.default_directory": r"C:\Projetos-Python\ministerio_da_cultura\repository\bronze\ministerio da cultura\{}".format(tipo_pessoa)
+    }
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", prefs)
+    #options.add_argument('--headless')
 
-res = requests.get(url=url, params=payload)
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+    # Inicializando o driver do Chrome
+    driver = webdriver.Chrome(options=options)
+
+    # URL do site
+    url = "http://sistemas.cultura.gov.br/salicnet/ctrIncentivadorMecenatoPorRegiaoUF/ctrIncentivadorMecenatoPorRegiaoUF.php"
+
+    # Iterando sobre os anos especificados
+    for ano in range(ano_ini, ano_final + 1):
+        msg = "Download ano " + str(ano)
+
+        # Abrindo a página no navegador
+        driver.get(url=url)
+        time.sleep(2)
+
+        # Selecionando o ano e o tipo de pessoa nos dropdowns
+        ano_element = driver.find_element(By.NAME, "ano")
+        select_ano = Select(ano_element)
+        select_ano.select_by_visible_text(str(ano))
+
+        tipo_pessoa_element = driver.find_element(By.NAME, "tipopessoa")
+        select_tipo_pessoa = Select(tipo_pessoa_element)
+        select_tipo_pessoa.select_by_visible_text(tipo_pessoa)
+
+        # Clicando no botão "OK"
+        btn_ok = driver.find_element(By.NAME, "sub_form").click()
+        time.sleep(1)
+
+        # Clicando no botão para baixar o arquivo XLS
+        btn_xls = driver.find_element(By.NAME, "sc_b_xls").click()
+
+        # Obtendo o link de download do arquivo
+        link_download = driver.find_element(By.XPATH, '/html/body/font/a/font/b').text
+        url_dowload = "http://sistemas.cultura.gov.br{}".format(link_download)
+
+        # Diretório base para salvar o arquivo
+        diretorio_base = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\bronze\ministerio da cultura"
+        nome_pasta = tipo_pessoa
+
+        # Caminho da pasta para o tipo de pessoa
+        path_pessoa_juridica = os.path.join(diretorio_base, nome_pasta)
+
+        # Verificando e criando a pasta se ela não existir
+        if not os.path.exists(path_pessoa_juridica):
+            os.makedirs(path_pessoa_juridica)
+
+        # Caminho completo do arquivo
+        caminho_arquivo = os.path.join(path_pessoa_juridica, str(ano) + '.xls')
+
+        # Baixando e salvando o arquivo
+        resp = requests.get(url_dowload)
+        with open(caminho_arquivo, 'wb') as arq:
+            arq.write(resp.content)
+        time.sleep(3)
+
+        # Retornando mensagem
+        yield msg
+
+    # Fechando o driver do Chrome
+    driver.quit()
+
+
+def transformar_dado_silver(tipo_pessoa):
+    diretorio_bronze = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\bronze\ministerio da cultura\{}".format(tipo_pessoa)
+    diretorio_silver = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\silver\ministerio da cultura\{}".format(tipo_pessoa)
+
+    # Lista todos os arquivos no diretório Excel
+    arquivos = os.listdir(diretorio_bronze)
+
+    for arquivo in arquivos:
+        msg = 'Transformando ano', arquivo.split('.')[0]
+        yield msg
+        # Lê o arquivo Excel
+        df = pd.read_excel(os.path.join(diretorio_bronze, arquivo))
+        # Altera nomes de colunas
+        df = df.rename(columns={
+            'CNPJ/CPF': 'CNPJ_CPF',
+            'Incentivador': 'INCENTIVADOR',
+            'NÂº Projeto': 'NUMERO_PROJETO',
+            'Nome do Projeto': 'NOME_PROJETO',
+            'UF do Projeto': 'UF_PROJETO',
+            'Vl. Incentivo R$': 'VALOR'
+        })
+
+        df['ANO'] = arquivo.split('.')[0]
+
+        arquivo_parquet = arquivo.split('.')[0] + '.parquet'
+
+        # Salva o DataFrame como arquivo Parquet
+        df.to_parquet(os.path.join(diretorio_silver, arquivo_parquet))
+
+
+def transformar_dado_gold(tipo_pessoa):
+    diretorio_silver = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\silver\ministerio da cultura\{}".format(tipo_pessoa)
+    diretorio_gold = r"C:\Users\DW8R\OneDrive - PETROBRAS\Documentos\projetos_python\ministerio_da_cultura\repository\gold\ministerio da cultura\{}".format(tipo_pessoa)
+
+    # Lista todos os arquivos no diretório Excel
+    arquivos = os.listdir(diretorio_silver)
+
+    arquivo_parquet = 'consolidado' + '.parquet'
+
+    dfs = []
+
+    for arquivo in arquivos:
+        # Ler o arquivo Parquet em um DataFrame
+        df = pd.read_parquet(os.path.join(diretorio_silver, arquivo))
+        # Adicionar o DataFrame à lista
+        dfs.append(df)
+
+        # Concatenar os DataFrames em um único DataFrame
+        df = pd.concat(dfs, ignore_index=True)
+        # Salva o DataFrame como arquivo Parquet
+        df.to_parquet(os.path.join(diretorio_gold, arquivo_parquet))
